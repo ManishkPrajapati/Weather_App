61f662684c500c66777f0f6cef7a309a32736e33
manish353730 <manishk353730@gmail.com>
2024-04-19 19:45:49 +0530

    weather app completed

---
+++ src/api.js
@@ -0,0 +1,16 @@
+
+
+export const GeoApioptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '10c4cd3e7fmshe37c8fe80f0f02fp1f03fajsn0e165e650bde',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URl = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY= "f5a1d2b8eddb546dcace08b3f5f4fa68";
+
